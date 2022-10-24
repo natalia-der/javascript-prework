d2fade583022763ef84902967a7e74919262130d
Natalia Derevianko <derevyanko.n7@gmail.com>
2022-10-24 17:35:35 +0200

    dodany computerMove

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+function printMessage(msg) {
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+    computerMove = 'papier'
+} else if (randomNumber == '3') {
+    computerMove = 'nożyce'
+}else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
