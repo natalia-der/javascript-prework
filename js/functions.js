96c5dab716cfb772b9cdefd26c52208f3edd40f2
Natalia Derevianko <derevyanko.n7@gmail.com>
2022-10-24 17:51:36 +0200

    dodany playerMove

--- js/functions.js
+++ js/functions.js
@@ -22,3 +22,17 @@ if (randomNumber == '1') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = parseFloat(prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'));
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'nożyce';
+} else if (playerInput == '3') {
+    playerMove = 'papier';
+} else {
+    playerMove = 'nieznany ruch';
+}
+  printMessage('Twój ruch: ' + playerMove);
